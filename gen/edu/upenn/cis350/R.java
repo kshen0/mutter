ae477ae15adcbab2664fdddc30dcb1d999c2cb97
Elissa Wolf <welissa@seas.upenn.du>
2012-02-23 20:35:19 -0500

    floorplan set as background, and circles can be drawn at any coordinates

--- gen/edu/upenn/cis350/R.java
+++ gen/edu/upenn/cis350/R.java
@@ -12,9 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int button=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int naboo=0x7f020002;
-        public static final int your=0x7f020003;
+        public static final int floorplan=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int naboo=0x7f020003;
+        public static final int your=0x7f020004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
