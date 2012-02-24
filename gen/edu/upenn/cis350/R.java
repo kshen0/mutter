f68943841e58aaa663d9aab57bf2a55760caa20b
Elissa Wolf <welissa@seas.upenn.du>
2012-02-24 14:47:37 -0500

    lolz

--- gen/edu/upenn/cis350/R.java
+++ gen/edu/upenn/cis350/R.java
@@ -12,10 +12,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int button=0x7f020000;
-        public static final int floorplan=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int naboo=0x7f020003;
-        public static final int your=0x7f020004;
+        public static final int button_off=0x7f020001;
+        public static final int button_on=0x7f020002;
+        public static final int floorplan=0x7f020003;
+        public static final int floorplan_alt=0x7f020004;
+        public static final int grid_view=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int naboo=0x7f020007;
+        public static final int your=0x7f020008;
     }
     public static final class layout {
         public static final int main=0x7f030000;
