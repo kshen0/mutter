56e4b6c044a6fbca804d210127ef2d06614f3b91
Kevin Shen <kwshen@gmail.com>
2012-03-11 05:17:15 -0400

    im gonna be so fucking tired tomorrow

--- gen/edu/upenn/cis350/R.java
+++ gen/edu/upenn/cis350/R.java
@@ -11,17 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int button=0x7f020000;
-        public static final int button_off=0x7f020001;
-        public static final int button_on=0x7f020002;
-        public static final int floorplan=0x7f020003;
-        public static final int floorplan_alt=0x7f020004;
-        public static final int floorplan_nb=0x7f020005;
-        public static final int grid_view=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int naboo=0x7f020008;
-        public static final int sample_exhibit=0x7f020009;
-        public static final int your=0x7f02000a;
+        public static final int aasoldier=0x7f020000;
+        public static final int button=0x7f020001;
+        public static final int button_off=0x7f020002;
+        public static final int button_on=0x7f020003;
+        public static final int floorplan=0x7f020004;
+        public static final int floorplan_alt=0x7f020005;
+        public static final int floorplan_nb=0x7f020006;
+        public static final int grid_view=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int letter=0x7f020009;
+        public static final int naboo=0x7f02000a;
+        public static final int parchment=0x7f02000b;
+        public static final int sample_exhibit=0x7f02000c;
+        public static final int your=0x7f02000d;
     }
     public static final class id {
         public static final int closebutton=0x7f050001;
@@ -31,9 +34,13 @@ public final class R {
         public static final int main=0x7f030000;
         public static final int pointselectdialog=0x7f030001;
         public static final int pointselectedlayout=0x7f030002;
+        public static final int popup=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int lipsum=0x7f040003;
+        public static final int ok=0x7f040002;
+        public static final int x=0x7f040004;
     }
 }
