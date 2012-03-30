7bb914feabfa07bd5198478f24b74ed55f117eeb
Kevin Shen <kwshen@gmail.com>
2012-03-30 16:35:46 -0400

    buttons wont go down

--- gen/edu/upenn/cis350/R.java
+++ gen/edu/upenn/cis350/R.java
@@ -17,17 +17,20 @@ public final class R {
         public static final int button_off_small_padded=0x7f020003;
         public static final int button_on_small=0x7f020004;
         public static final int floorplan2e=0x7f020005;
-        public static final int floorplan_nb=0x7f020006;
-        public static final int floorplan_path2=0x7f020007;
-        public static final int hancock_cornelia_portrait=0x7f020008;
-        public static final int hospital1=0x7f020009;
-        public static final int hospital2=0x7f02000a;
-        public static final int ic_launcher=0x7f02000b;
-        public static final int letter=0x7f02000c;
-        public static final int parchment=0x7f02000d;
-        public static final int parchment2=0x7f02000e;
-        public static final int sbutton=0x7f02000f;
-        public static final int sdl=0x7f020010;
+        public static final int floorplan_blue_small=0x7f020006;
+        public static final int floorplan_nb=0x7f020007;
+        public static final int floorplan_path2=0x7f020008;
+        public static final int floorplan_path2_small=0x7f020009;
+        public static final int hancock_cornelia_portrait=0x7f02000a;
+        public static final int hospital1=0x7f02000b;
+        public static final int hospital2=0x7f02000c;
+        public static final int ic_launcher=0x7f02000d;
+        public static final int letter=0x7f02000e;
+        public static final int openscreen_small=0x7f02000f;
+        public static final int parchment=0x7f020010;
+        public static final int parchment2=0x7f020011;
+        public static final int sbutton=0x7f020012;
+        public static final int sdl=0x7f020013;
     }
     public static final class id {
         public static final int AASoldier_button=0x7f060005;
