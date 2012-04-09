1af3792482b6625b18c231444a1fcd29b040f047
Elissa <welissa@seas.upenn.edu>
2012-04-09 00:04:16 -0400

    audio refactored, broken nurse point fixed

--- src/edu/upenn/cis350/SelectPointActivity.java
+++ src/edu/upenn/cis350/SelectPointActivity.java
@@ -15,13 +15,12 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class SelectPointActivity extends Activity {
-	
-	MediaPlayer mp;
-    MediaPlayer mp2;
-    
-    int l = 0;
-	
-	
+
+	MediaPlayer mp;  //Cornelia
+	MediaPlayer mp2;   //manley stacy
+
+	int l = 0;
+
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,131 +30,8 @@ public class SelectPointActivity extends Activity {
         l = b.getInt("layout");
         setContentView(l);
         
-        if (l == R.layout.point3layout) {
-        	mp = MediaPlayer.create(SelectPointActivity.this, R.raw.a2);
-        	
-        	Button cmd_play1 = (Button)this.findViewById(R.id.audio_button);
-	        cmd_play1.setOnClickListener(new OnClickListener(){
-	        
-	           	@Override
-	            public void onClick(View arg0) {
-	            	try {
-	                    Log.v("Status:", "Can you hear me now?");
-	                    
-	                    if (!mp.isPlaying()) {
-	                    	mp.seekTo(0);
-	                    	mp.start();
-	                    }
-	                    else if (mp.isPlaying()) {
-	                    	mp.seekTo(0);
-	                    	mp.stop();
-	                    	mp.prepare();
-	                    }
-	                    
-	                    
-	                    mp.setOnCompletionListener(new OnCompletionListener(){
-	                        public void onCompletion(MediaPlayer arg0) {
-	                            
-	                        }
-	                    });
-	                    
-	            	}
-	            	catch (Exception e) {
-	                	Log.e("err", "error: " + e.getMessage(), e);
-	                }
-	            }
-	        });
-        }
-        
-        if (l == R.layout.pointselectedlayout) {
-	        mp = MediaPlayer.create(SelectPointActivity.this, R.raw.a1);
-	        mp2 = MediaPlayer.create(SelectPointActivity.this, R.raw.a2);
-	        
-	        //AUDIO 1 button
-	        Button cmd_play1 = (Button)this.findViewById(R.id.audio_button);
-	        cmd_play1.setOnClickListener(new OnClickListener(){
-	        
-	           	@Override
-	            public void onClick(View arg0) {
-	            	try {
-	            		Log.v("Status:", "Playing that funky music, white boy");
-	                    //MediaPlayer mp = MediaPlayer.create(SelectPointActivity.this, R.raw.a1);
-	                    Log.v("Status:", "Media loaded");
-	                    //mp.prepare();
-	                    //mp.start();
-	                    
-	                    Log.v("Status:", "Can you hear me now?");
-	                    
-	                    if (mp2.isPlaying()) {
-	                    	mp2.seekTo(0);
-	                    	mp2.stop();
-	                    	mp2.prepare();
-	                    }
-	                    if (!mp.isPlaying()) {
-	                    	mp.seekTo(0);
-	                    	mp.start();
-	                    }
-	                    else if (mp.isPlaying()) {
-	                    	mp.seekTo(0);
-	                    	mp.stop();
-	                    	mp.prepare();
-	                    }
-	                    
-	                    
-	                    mp.setOnCompletionListener(new OnCompletionListener(){
-	                        public void onCompletion(MediaPlayer arg0) {
-	                            
-	                        }
-	                    });
-	                    
-	            	}
-	            	catch (Exception e) {
-	                	Log.e("err", "error: " + e.getMessage(), e);
-	                }
-	            }
-	        });
-	        
-	        //AUDIO 2 BUTTON
-	        Button cmd_play = (Button)this.findViewById(R.id.audio_button2);
-	        cmd_play.setOnClickListener(new OnClickListener(){
-	        	@Override
-	            public void onClick(View arg0) {
-	            	try {
-	            		Log.v("Status:", "Playing that funky music, white boy");
-	                    //MediaPlayer mp = MediaPlayer.create(SelectPointActivity.this, R.raw.a2);
-	                    Log.v("Status:", "Media loaded");
-	                    
-	                    Log.v("Status:", "Can you hear me now?");
-	                    
-	                    if (mp.isPlaying()) {
-	                    	mp.seekTo(0);
-	                    	mp.stop();
-	                    	mp.prepare();
-	                    }
-	                    if (!mp2.isPlaying()) {
-	                    	mp2.seekTo(0);
-	                    	mp2.start();
-	                    }
-	                    else if (mp2.isPlaying()) {
-	                    	mp2.seekTo(0);
-	                    	mp2.stop();
-	                    	mp2.prepare();
-	                    }
-	                    
-	                    
-	                    mp.setOnCompletionListener(new OnCompletionListener(){
-	                        public void onCompletion(MediaPlayer arg0) {
-	                            
-	                        }
-	                    });
-	                    
-	            	}
-	            	catch (Exception e) {
-	                	Log.e("err", "error: " + e.getMessage(), e);
-	                }
-	            }
-	        }); 
-        }
+        mp = MediaPlayer.create(SelectPointActivity.this, R.raw.a1); //Cornelia
+    	mp2 = MediaPlayer.create(SelectPointActivity.this, R.raw.a2);  //manley stacy
         
         if (l == R.layout.pointlayouttwo) {
         	new CountDownTimer(3000, 1000) {
@@ -237,16 +113,89 @@ public class SelectPointActivity extends Activity {
         }
 	}
 	
-	public void onCloseClick(View view){
-		if (l == R.layout.pointselectedlayout || l == R.layout.point3layout) {
+	public void onClickManleyStacy(View view){
+		//mp = MediaPlayer.create(SelectPointActivity.this, R.raw.a2);
+		try {
+            Log.v("Status:", "Can you hear me now?");
+            
+            if (mp.isPlaying()) {
+            	mp.seekTo(0);
+            	mp.stop();
+            	mp.prepare();
+            }
+            if (!mp2.isPlaying()) {
+            	mp2.seekTo(0);
+            	mp2.start();
+            }
+            else if (mp2.isPlaying()) {
+            	mp2.seekTo(0);
+            	mp2.stop();
+            	mp2.prepare();
+            }
+            mp2.setOnCompletionListener(new OnCompletionListener(){
+                public void onCompletion(MediaPlayer arg0) {
+                    
+                }
+            }); 
+    	}
+    	catch (Exception e) {
+        	Log.e("err", "error: " + e.getMessage(), e);
+        }
+    }
+	
+	public void onClickCornelia(View view){
+		
+		//mp = MediaPlayer.create(SelectPointActivity.this, R.raw.a1);
+        //mp2 = MediaPlayer.create(SelectPointActivity.this, R.raw.a2);
+		
+		try {
+    		Log.v("Status:", "Playing that funky music, white boy");
+            //MediaPlayer mp = MediaPlayer.create(SelectPointActivity.this, R.raw.a1);
+            Log.v("Status:", "Media loaded");
+            //mp.prepare();
+            //mp.start();
+            
+            Log.v("Status:", "Can you hear me now?");
+            
+            if (mp2.isPlaying()) {
+            	mp2.seekTo(0);
+            	mp2.stop();
+            	mp2.prepare();
+            }
+            if (!mp.isPlaying()) {
+            	mp.seekTo(0);
+            	mp.start();
+            }
+            else if (mp.isPlaying()) {
+            	mp.seekTo(0);
+            	mp.stop();
+            	mp.prepare();
+            }
+            
+            
+            mp.setOnCompletionListener(new OnCompletionListener(){
+                public void onCompletion(MediaPlayer arg0) {
+                    
+                }
+            });
+            
+    	}
+    	catch (Exception e) {
+        	Log.e("err", "error: " + e.getMessage(), e);
+        }
+    
+	}
+
+	public void onCloseClick(View view) {
+		//if (l == R.layout.pointselectedlayout || l == R.layout.point3layout) {
 			if (mp.isPlaying()) {
 				mp.stop();
 			}
-		if (l == R.layout.pointselectedlayout)
-			if (mp2.isPlaying()) {
-				mp2.stop();
-			}
-		}
+			//if (l == R.layout.pointselectedlayout)
+				if (mp2.isPlaying()) {
+					mp2.stop();
+				}
+		//}
 		finish();
 	}
 
