4eabc2c94a5fff1db5dc97ff382cb7f9769b4705
Kevin Shen <kwshen@gmail.com>
2012-04-27 15:58:18 -0400

    calibrated for phone

--- src/edu/upenn/cis350/MutterActivity.java
+++ src/edu/upenn/cis350/MutterActivity.java
@@ -14,7 +14,7 @@ public class MutterActivity extends Activity {
 	// map containing key: layout id, value: int array of 3 values (x, y, button
 	// side length)
 	private HashMap<Integer, ArrayList<Integer>> pointCoords;
-	private static final int SIDE = 30;
+	private static final int SIDE = 37;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -81,27 +81,27 @@ public class MutterActivity extends Activity {
 		ArrayList<Integer> points = new ArrayList<Integer>();
 		if (ID == 0) {  //Nurse
 			points.clear();
-			points.add(173); //P1 x
-			points.add(240); //P1 y
+			points.add(178); //P1 x
+			points.add(246); //P1 y
 			points.add(1); //P1 iconID
 			points.add(R.layout.nursepoint); //P1 layout
-			points.add(145); //P2 x
-			points.add(410); //P2 y
+			points.add(149); //P2 x
+			points.add(424); //P2 y
 			points.add(0); //P2 iconID
 			points.add(R.layout.pointlayouttwo);
 			points.add(10);  //P3 x
-			points.add(152); //P3 y
+			points.add(158); //P3 y
 			points.add(1); //P3 iconID
 			points.add(R.layout.point3layout);
 			pointCoords.put(R.layout.nurselayout, points);
 		}
 		if (ID == 1){ //African American Soldier
-			points.add(170); //P1 x
-			points.add(220); //P1 y
+			points.add(312); //P1 x
+			points.add(445); //P1 y
 			points.add(1);
 			points.add(R.layout.pointselectedlayout); 
-			points.add(50);
-			points.add(50);
+			points.add(62);
+			points.add(80);
 			points.add(0);
 			points.add(R.layout.aa_point_two);
 			
